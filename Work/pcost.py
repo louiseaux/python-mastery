4998cefa1f90c9c5a6f0e5671a7576f4512587ef
Ashlyn <ashlyn.barry1@gmail.com>
2025-05-06 12:44:04 +1000

    calculated portfolio cost (ex 1.3)

---
+++ Work/pcost.py
@@ -0,0 +1,14 @@
+# pcost.py
+#
+# Exercise 1.3
+
+total_cost = 0.0
+
+with open('../Data/portfolio.dat', 'r') as f:
+    for line in f:
+        fields = line.split()
+        nshares = int(fields[1])
+        price = float(fields[2])
+        total_cost += nshares * price
+
+print(total_cost)
