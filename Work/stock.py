a4b737eaa2e433b7ee47d80e65c26ad1d8286fe9
Ashlyn <ashlyn.barry1@gmail.com>
2025-05-06 13:09:52 +1000

    defined stock class (ex 1.5)

---
+++ Work/stock.py
@@ -0,0 +1,12 @@
+# stock.py
+#
+# Exercise 1.5
+
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
