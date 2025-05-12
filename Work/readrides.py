fdc28040172e4cee90f602fa8b81d117aad1501e
Ashlyn <ashlyn.barry1@gmail.com>
2025-05-12 11:09:42 +1000

    created a custom container (ex 2.5)

--- Work/readrides.py
+++ Work/readrides.py
@@ -2,6 +2,7 @@
 #
 # Exercise 2.1
 
+import collections
 import csv
 
 def read_rides_as_tuples(filename):
@@ -11,7 +12,7 @@ def read_rides_as_tuples(filename):
     records = []
     with open(filename) as f:
         rows = csv.reader(f)
-        headings = next(rows)   # Skip headers
+        headings = next(rows)    # Skip headers
         for row in rows:
             route = row[0]
             date = row[1]
@@ -25,10 +26,10 @@ def read_rides_as_dicts(filename):
     '''
     Read the bus ride data as a list of dicts
     '''
-    records = []
+    records = RideData()
     with open(filename) as f:
         rows = csv.reader(f)
-        headings = next(rows)   # Skip headers
+        headings = next(rows)    # Skip headers
         for row in rows:
             route = row[0]
             date = row[1]
@@ -45,7 +46,7 @@ def read_rides_as_dicts(filename):
 
 class Row:
     # Uncomment to see effect of slots
-    __slots__ = ['route', 'date', 'daytype', 'rides']
+    __slots__ = ('route', 'date', 'daytype', 'rides')
     def __init__(self, route, date, daytype, rides):
         self.route = route
         self.date = date
@@ -63,7 +64,7 @@ def read_rides_as_instances(filename):
     records = []
     with open(filename) as f:
         rows = csv.reader(f)
-        headings = next(rows)   # Skip headers
+        headings = next(rows)    # Skip headers
         for row in rows:
             route = row[0]
             date = row[1]
@@ -73,10 +74,52 @@ def read_rides_as_instances(filename):
             records.append(record)
     return records
 
+def read_rides_as_columns(filename):
+    '''
+    Read the bus ride data into 4 lists, representing columns
+    '''
+    routes = []
+    dates = []
+    daytypes = []
+    numrides = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)    # Skip headers
+        for row in rows:
+            routes.append(row[0])
+            dates.append(row[1])
+            daytypes.append(row[2])
+            numrides.append(int(row[3]))
+    return dict(routes=routes, dates=dates, daytypes=daytypes, numrides=numrides)
+
+class RideData(collections.abc.Sequence):
+    def __init__(self):
+        # Each value is a list with all the values (a column)
+        self.routes = []
+        self.dates = []
+        self.daytypes = []
+        self.numrides = []
+
+    def __len__(self):
+        # All lists assumed to have the same length
+        return len(self.routes)
+    
+    def __getitem__(self, index):
+        return { 'route': self.routes[index],
+                 'date': self.dates[index],
+                 'daytype': self.daytypes[index],
+                 'rides': self.numrides[index] }
+    
+    def append(self, d):
+        self.routes.append(d['route'])
+        self.dates.append(d['date'])
+        self.daytypes.append(d['daytype'])
+        self.numrides.append(d['rides'])
+
 if __name__ == '__main__':
     import tracemalloc
     tracemalloc.start()
-    read_rides = read_rides_as_instances # Change to as_dicts, as_instances, etc.
+    read_rides = read_rides_as_dicts # Change to as_dicts, as_instances, etc.
     rides = read_rides('../Data/ctabus.csv')
 
     print('Memory Use: Current %d, Peak %d' % tracemalloc.get_traced_memory())
