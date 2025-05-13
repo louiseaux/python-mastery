27c1b8a05753781ad3dff606fe3b1c102f57ab39
Ashlyn <ashlyn.barry1@gmail.com>
2025-05-13 11:45:24 +1000

    created class method (ex 3.3)

--- Work/stock.py
+++ Work/stock.py
@@ -3,13 +3,20 @@
 # Exercise 1.5
 
 import csv
+import reader
 import tableformat
 
 class Stock:
+    types = (str, int, float)
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
 
     def cost(self):
         return self.shares * self.price
@@ -26,10 +33,10 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            record = Stock(row[0], int(row[1]), float(row[2]))
+            record = Stock.from_row(row)
             portfolio.append(record)
     return portfolio
 
 if __name__ == '__main__':
-    portfolio = read_portfolio('../Data/portfolio.csv')
+    portfolio = reader.read_csv_as_instances('../Data/portfolio.csv', Stock)
     tableformat.print_table(portfolio, ['name', 'shares', 'price'])
--- Work/tableformat.py
+++ Work/tableformat.py
@@ -3,7 +3,12 @@
 # Exercise 3.2
 
 def print_table(records, fields):
+    # Print the table headers in a 10-character wide field
     print(' '.join('%10s' % fieldname for fieldname in fields))
+
+    # Print the separator bars
     print(('-'*10 + ' ')*len(fields))
+
+    # Output the table contents
     for record in records:
         print(' '.join('%10s' % getattr(record, fieldname) for fieldname in fields))
