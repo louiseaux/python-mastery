b5786414d3db5c6dc7407e96ebaa71482a6ac3bc
Ashlyn <ashlyn.barry1@gmail.com>
2025-05-13 12:30:13 +1000

    enforced validation rules (ex 3.4)

--- Work/stock.py
+++ Work/stock.py
@@ -2,12 +2,9 @@
 #
 # Exercise 1.5
 
-import csv
-import reader
-import tableformat
-
 class Stock:
-    types = (str, int, float)
+    __slots__ = ('name', '_shares', '_price')
+    _types = (str, int, float)
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -15,28 +12,36 @@ class Stock:
 
     @classmethod
     def from_row(cls, row):
-        values = [func(val) for func, val in zip(cls.types, row)]
+        values = [func(val) for func, val in zip(cls._types, row)]
         return cls(*values)
+    
+    @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, self._types[1]):
+            raise TypeError(f'Expected {self._types[1].__name__}')
+        if value < 0:
+            raise ValueError('shares must be >= 0')
+        self._shares = value
 
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, self._types[2]):
+            raise TypeError(f'Expected {self._types[2].__name__}')
+        if value < 0:
+            raise ValueError('price must be >= 0')
+        self._price = value
+
+    @property
     def cost(self):
         return self.shares * self.price
     
     def sell(self, nshares):
         self.shares -= nshares
-
-def read_portfolio(filename):
-    '''
-    Read a CSV file of stock data into a list of Stocks
-    '''
-    portfolio = []
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            record = Stock.from_row(row)
-            portfolio.append(record)
-    return portfolio
-
-if __name__ == '__main__':
-    portfolio = reader.read_csv_as_instances('../Data/portfolio.csv', Stock)
-    tableformat.print_table(portfolio, ['name', 'shares', 'price'])
