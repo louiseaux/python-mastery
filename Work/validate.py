e3020b74d565fa64d8f41cbc26106f4665c36a93
Ashlyn <ashlyn.barry1@gmail.com>
2025-05-15 13:37:54 +1000

    defined descriptor class (ex 4.3)

--- Work/validate.py
+++ Work/validate.py
@@ -3,9 +3,18 @@
 # Exercise 4.2
 
 class Validator:
+    def __init__(self, name=None):
+        self.name = name
+
+    def __set_name__(self, cls, name):
+        self.name = name
+
     @classmethod
     def check(cls, value):
         return value
+    
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = self.check(value)
     
 class Typed(Validator):
     expected_type = object
