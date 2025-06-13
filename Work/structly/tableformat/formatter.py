eaab7c6b7e39a30a06b1ae447150ae16e49f620e
Ashlyn <ashlyn.barry1@gmail.com>
2025-06-13 13:39:53 +1000

    add dynamic imports (ex 9.4)

--- Work/structly/tableformat/formatter.py
+++ Work/structly/tableformat/formatter.py
@@ -19,6 +19,13 @@ def print_table(records, fields, formatter):
         formatter.row(rowdata)
 
 class TableFormatter(ABC):
+    _formats = { }
+
+    @classmethod
+    def __init_subclass__(cls):
+        name = cls.__module__.split('.')[-1]
+        TableFormatter._formats[name] = cls
+
     @abstractmethod
     def headings(self, headers):
         pass
@@ -26,10 +33,6 @@ class TableFormatter(ABC):
     @abstractmethod
     def row(self, rowdata):
         pass
-
-from .formats.text import TextTableFormatter
-from .formats.csv import CSVTableFormatter
-from .formats.html import HTMLTableFormatter
 
 class ColumnFormatMixin:
     formats = []
@@ -42,13 +45,11 @@ class UpperHeadersMixin:
         super().headings([ h.upper() for h in headers ])
 
 def create_formatter(name, column_formats=None, upper_headers=None):
-    if name == 'text':
-        formatter_cls = TextTableFormatter
-    elif name == 'csv':
-        formatter_cls = CSVTableFormatter
-    elif name == 'html':
-        formatter_cls = HTMLTableFormatter
-    else:
+    if name not in TableFormatter._formats:
+        __import__(f'{__package__}.formats.{name}')
+
+    formatter_cls = TableFormatter._formats.get(name)
+    if not formatter_cls:
         raise RuntimeError('Unknown format %s' % name)
     
     if column_formats:
@@ -58,5 +59,5 @@ def create_formatter(name, column_formats=None, upper_headers=None):
     if upper_headers:
         class formatter_cls(UpperHeadersMixin, formatter_cls):
             pass
-
+    
     return formatter_cls()
