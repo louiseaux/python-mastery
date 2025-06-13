eebd8283a6fa13ace38812c75eb776554a0f9ae3
Ashlyn <ashlyn.barry1@gmail.com>
2025-06-13 12:33:22 +1000

    review module loading (ex 9.1)

---
+++ Work/simplemod.py
@@ -0,0 +1,17 @@
+# simplemod.py
+#
+# Exercise 9.1
+
+x = 42    # A global variable
+
+# A simple function
+def foo():
+    print('x is', x)
+
+# A simple class
+class Spam:
+    def yow(self):
+        print('Yow!')
+
+# A scripting statement
+print('Loaded simplemod')
